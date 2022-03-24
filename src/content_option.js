38a3bfe88768438b0017d7c6dffef8df34ff0598
keyguru65748 <webpythonexpert@gmail.com>
2022-03-24 17:16:30 +0900

    react portfolio template

---
+++ src/content_option.js
@@ -0,0 +1,232 @@
+const logotext = "HINATA";
+const meta = {
+    title: "Hinata Ito",
+    description: "I’m Hinata Ito Blockchain & Full Stack devloper,currently working in Remotely",
+};
+
+const introdata = {
+    title: "I’m Hinata Ito",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "Passionate and skilled Full Stack & Blockchain Developer with 6 years of experience in building projects for various sized businesses",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Senior Blockchain & Full Stack Developer with solid experience. Worked on several companies with great teams. During the days, I have build many web applications and blockchain services. I have phenomental time management skills and a strong user focus with a can-do attitude. In reality, I always push myself to create high-quality work while keeping an eye for the most minute details to ensure a product that won't fail. I support proficient problem-solving, assist clients from project architecture to fruition",
+};
+const worktimeline = [{
+    jobtitle: "Front End Developer",
+    where: "Tacchi Studios",
+    date: "2016-2017",
+},
+{
+    jobtitle: "Full Stack Developer",
+    where: "Ikayzo",
+    date: "2017-2018",
+},
+{
+    jobtitle: "Full Stack & BLockchain Developer",
+    where: "QBurst",
+    date: "2018-2020",
+},
+{
+    jobtitle: "Blockchain Developer",
+    where: "Ekoios Technology",
+    date: "2020-2022",
+},
+];
+
+const skills = [{
+    name: "React",
+    value: 90,
+},
+{
+    name: "Typescript",
+    value: 95,
+},
+{
+    name: "Angular.js",
+    value: 85,
+},
+
+{
+    name: "Vue.js",
+    value: 90,
+},
+{
+    name: "Next.js",
+    value: 87,
+},
+{
+    name: "Node.js",
+    value: 83,
+},
+{
+    name: "Express.js",
+    value: 80,
+},
+{
+    name: "Django",
+    value: 80,
+},
+{
+    name: "SQL",
+    value: 90,
+},
+{
+    name: "MySql",
+    value: 93,
+},
+{
+    name: "MongoDB",
+    value: 88,
+},
+{
+    name: "PostgreSQL",
+    value: 82,
+},
+{
+    name: "CodeIgniter",
+    value: 91,
+},
+{
+    name: "Laravel",
+    value: 87,
+},
+{
+    name: "GoLang",
+    value: 83,
+},
+{
+    name: "Rust",
+    value: 85,
+},
+{
+    name: "Solana",
+    value: 87,
+},
+{
+    name: "Solidity",
+    value: 85,
+},
+{
+    name: "Truffle",
+    value: 87,
+},
+{
+    name: "Hardhat",
+    value: 90,
+},
+{
+    name: "Smart contract",
+    value: 88,
+},
+{
+    name: "NFT",
+    value: 85,
+},
+];
+
+const services = [{
+    title: "Customer Service",
+    description: "Always put the customer's interests first"
+},
+{
+    title: "Problem Solving",
+    description: "Complete all problems without even the slightest mistake"
+},
+{
+    title: "Time Management",
+    description: "Deploy projects on time"
+},
+{
+    title: "Team Work",
+    description: "Best team spirit and collaboration"
+},
+{
+    title: "Critical Thinking",
+    description: "Provide new and perfect idea to customer"
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "One-stop study abroad service, focusing on studying in Australia",
+    link: "https://www.oneuedu.com/",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "SCRUM FEST Osaka 2022 @online",
+    link: "https://www.scrumosaka.org/",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: " Katsuhito Nakajima",
+    link: "https://www.katsuhito-nakajima-yamanashi.com/",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "Your thoughts are in full bloom. For gifts to loved ones. preserved flower",
+    link: "https://www.olive-flower.com/",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "MABUIOTO Soul Music Festival Mabuioto",
+    link: "https://www.mabuioto.com/",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "LAB surf",
+    link: "https://www.labsurf.com/",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "SECRET 7 LINE",
+    link: "https://secret7line.com/",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "editorialrepublica",
+    link: "https://www.ed-republica.com/",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "Connect smiles with deliciousness",
+    link: "https://www.kskure.com/",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "hinata.keyguru@gmail.com",
+    YOUR_FONE: "(81)195506181",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/keyguru65748",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    // twitter: "https://twitter.com",
+    // youtube: "https://youtube.com",
+    // twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
